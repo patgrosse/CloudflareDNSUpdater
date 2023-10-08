486085b7889df48f930b822786996301c7279326
Patrick Große <patrick.pgrosse@gmail.com>
2023-10-08 21:46:56 +0200

    Update Python projects; version bump 0.5.0

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup
 
 setup(name="CloudFlareDNSUpdater",
-      version="0.4.1",
+      version="0.5.0",
       description="Update IPv4 or IPv6 record according to external address of current machine",
       author="Patrick Grosse",
       author_email="patrick.pgrosse@gmail.com",
       packages=["cfdnsupdater"],
       scripts=["scripts/updatecfdns"],
       python_requires='>=3',
-      install_requires=["CloudFlare==2.8.15", "pyroute2==0.6.5", "requests==2.31.0", "configparser==5.1.0"]
+      install_requires=["CloudFlare==2.12.3", "pyroute2==0.7.3", "requests==2.31.0", "configparser==6.0.0"]
       )
